d5b1d82df4816f6efbcda4c00fe354a69b214327
FatherDivine <aaron.staten@ucdenver.edu>
2024-05-17 14:54:26 -0600

    New ssid

--- wifispam.h
+++ wifispam.h
@@ -63,6 +63,7 @@ const char funnyssids[] PROGMEM = {
   "Life in the Fast LAN\n"
   "The Creep Next Door\n"
   "Ye Olde Internet\n"
+  "CIA-spypigeon6\n"
 };
 
 const char rickrollssids[] PROGMEM = {
